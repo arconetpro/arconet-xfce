28a1f9eb522b6873fbe72e4a33dec8edc1a239c1
arcolinuxz <arcolinuxinfo@gmail.com>
2024-06-27 14:46:44 +0200

    update

--- etc/skel/.config/xfce4/terminal/accels.scm
+++
@@ -1,3 +0,0 @@
-(gtk_accel_path "<Actions>/terminal-window/zoom-reset" "<Primary>0")
-(gtk_accel_path "<Actions>/terminal-window/zoom-out" "<Primary>minus")
-(gtk_accel_path "<Actions>/terminal-window/zoom-in" "<Primary>equal")
